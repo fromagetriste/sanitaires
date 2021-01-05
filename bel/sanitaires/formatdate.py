80cf481e655ccb059809342ebd6acc3c45fa21c2
fromagetriste <fromagetriste@gmail.com>
2021-01-05 23:43:08 +0100

    changement calculs dates

--- bel/sanitaires/formatdate.py
+++ bel/sanitaires/formatdate.py
@@ -1,9 +1,14 @@
 import datetime
 from datetime import datetime
 
+
+# sert à changer les 05/01/21 en 05/01/2021 pour la date chargement et la DLUO
 # date_pandas = "05/01/21" (ex : date de chargement ou DLUO)
 
 def format_europeen(date_pandas):
-    ma_date = str(date_pandas)
-    ma_date = datetime.strptime(ma_date, '%d/%m/%y')
-    return ma_date.strftime('%d/%m/%Y')
+    try:
+        ma_date = str(date_pandas)
+        ma_date = datetime.strptime(ma_date, '%d/%m/%y')
+        return ma_date.strftime('%d/%m/%Y')
+    except:
+        pass
--- bel/sanitaires/quantieme.py
+++ bel/sanitaires/quantieme.py
@@ -5,6 +5,41 @@ from datetime import datetime
 # lot = "SC2941D" (quantième 294, soit 20 Octobre, servira d'exemple)
 # -------------------------------------------------------------------
 
+
+def calcul_date_prod(lot):
+    
+    lot_premier_caractere = str(lot[0]) 
+    chiffres = "0123456789"
+    date_auj = datetime.now()
+
+
+    if lot_premier_caractere not in chiffres: 
+
+        try:
+            quantieme = str(lot[2:-2])
+            annee_auj = str(datetime.now().year)
+            quantieme_et_annee_auj = quantieme + "/" + annee_auj
+            lot_un = datetime.strptime(quantieme_et_annee_auj, '%j/%Y')
+            
+            if date_auj > lot_un:
+                return lot_un.strftime('%d/%m/%Y')
+                
+            else:
+                annee_auj_moins_un = int(annee_auj) - 1
+                quantieme_et_annee_moins_un = quantieme + "/" + str(annee_auj_moins_un)
+                lot_deux = datetime.strptime(quantieme_et_annee_moins_un, '%j/%Y')
+                return lot_deux.strftime('%d/%m/%Y')   
+
+        except:
+            return("MMBE")
+            
+    else:
+        return("MMBE")
+
+
+
+
+'''
 def calcul_date_prod(lot):
     lot_premier_caractere = str(lot[0]) # premier élément du lot
     chiffres = "0123456789"
@@ -35,4 +70,4 @@ def calcul_date_prod(lot):
             return("MMBE")
     else:
         return("MMBE")
-
+'''
