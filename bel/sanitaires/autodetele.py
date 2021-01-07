d50a203a3ae9265086d4ab006454d8a1fd725d5a
fromagetriste <fromagetriste@gmail.com>
2021-01-07 17:00:58 +0100

    adding autodelete

---
+++ bel/sanitaires/autodetele.py
@@ -0,0 +1,18 @@
+
+import os
+import path
+
+# pour supprimer tous les fichiers .txt et .xlsx stockés dans MEDIA, car 512Mo max
+# to be run as a daily task by pythonanywhere
+
+def main():
+    try:
+        dir = '/home/bel/bel.pythonanywhere.com/bel/uploads/uploads'
+        for f in os.listdir(dir):
+            os.remove(os.path.join(dir, f))
+    except:
+        pass
+
+
+if __name__ == "__main__":
+    main()
