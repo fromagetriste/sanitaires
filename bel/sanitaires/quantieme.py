3e9bc5310885f90044876d8dc1f539f6632596cc
fromagetriste <fromagetriste@gmail.com>
2021-01-10 16:17:53 +0100

    adding favicon

--- bel/sanitaires/quantieme.py
+++ bel/sanitaires/quantieme.py
@@ -18,10 +18,10 @@ def calcul_date_prod(lot):
             quantieme = str(lot[2:-2]) # on isole 294
             annee_auj = str(datetime.now().year) # = 2021
             quantieme_et_annee_auj = quantieme + "/" + annee_auj   # on obtient "294/2021"
-            lot_un = datetime.strptime(quantieme_et_annee_auj, '%j/%Y') #%j = day of the year
+            lot_un = datetime.strptime(quantieme_et_annee_auj, '%j/%Y') #%j = day of the year, on crée l'objet date
             
             if date_auj > lot_un:
-                return lot_un.strftime('%d/%m/%Y') # on recrée en objet date avec le format voulu
+                return lot_un.strftime('%d/%m/%Y') # on repasse en objet str avec le format voulu
                 
             else: # pour les dates de prod postérieures à la dates du jour
                 annee_auj_moins_un = int(annee_auj) - 1
